3ac01394e49938a08a0819fa62c64d2ffbe6a96e
rouancon <rouan.c@husky.neu.edu>
2019-03-28 12:28:46 -0400

    data set

--- stocks.r
+++ stocks.r
@@ -1,1 +1,14 @@
 #Stock price prediction
+
+#import data
+apple_train <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "AAPL-train")
+apple_val <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "AAPL-val")
+apple_model <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "AAPL-model")
+goog_train <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "GOOG-train")
+goog_val <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "GOOG-val")
+goog_model <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "GOOG-model")
+xom_train <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "XOM-train")
+xom_val <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "XOM-val")
+xom_model <- read_excel("Documents/GitHub/stock-prediction/prices-split-adjusted.xlsx", sheet = "XOM-model")
+
+
